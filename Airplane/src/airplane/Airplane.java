c4cb0ea964330ac0627bf9f848df6f1edd110696
Misindexed Orange Hat <Konnoke@users.noreply.github.com>
2016-03-03 00:33:42 -0500

    Added files via upload

---
+++ Airplane/src/airplane/Airplane.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package airplane;
+
+/**
+ *
+ * @author Konnoke
+ */
+public class Airplane {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+    
+    
+    }
+    
+    public void run(){
+        
+        
+    }
+    
+    
+    
+}
